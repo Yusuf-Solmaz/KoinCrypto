3b0d098f4599e3d337958479685a273337bb0359
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-08-21 18:05:46 +0300

    BASE_URL and API_KEY

---
+++ app/src/main/java/com/yusuf/koincryptoapp/util/API.kt
@@ -0,0 +1,6 @@
+package com.yusuf.koincryptoapp.util
+
+object API {
+    const val BASE_URL:String = "https://raw.githubusercontent.com/"
+    const val API_KEY:String= "Yusuf-Solmaz/CryptoCurrencyApp/master/cyptoCurrencies.json"
+}
