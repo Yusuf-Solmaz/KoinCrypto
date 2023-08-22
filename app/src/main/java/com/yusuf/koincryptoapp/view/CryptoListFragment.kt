b7ccf7ea7355b210549815abe07910cdc63237f3
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-08-22 11:34:20 +0300

    Update

--- app/src/main/java/com/yusuf/koincryptoapp/view/CryptoListFragment.kt
+++ app/src/main/java/com/yusuf/koincryptoapp/view/CryptoListFragment.kt
@@ -1,11 +1,13 @@
 package com.yusuf.koincryptoapp.view
 
+import android.opengl.Visibility
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yusuf.koincryptoapp.databinding.FragmentCryptoListBinding
@@ -42,7 +44,47 @@ class CryptoListFragment : Fragment() , RecyclerViewAdapter.Listener{
         binding.recyclerView.layoutManager = layoutManager
 
         viewModel = ViewModelProvider(this).get(CryptoViewModel::class.java)
+
+        observeLiveData()
+
         viewModel.getDataFromAPI()
+    }
+
+    private fun observeLiveData(){
+        viewModel.cryptoList.observe(viewLifecycleOwner, Observer {cryptos ->
+            cryptos?.let{
+                binding.recyclerView.visibility=View.VISIBLE
+                cryptoAdapter = RecyclerViewAdapter(ArrayList(cryptos.data ?: arrayListOf()),this@CryptoListFragment)
+                binding.recyclerView.adapter=cryptoAdapter
+            }
+        })
+
+        viewModel.cryptoError.observe(viewLifecycleOwner, Observer { error->
+
+            error?.let {
+                if (it.data == true){
+                    binding.cryptoErrorText.visibility=View.VISIBLE
+                    binding.recyclerView.visibility=View.GONE
+                }
+                else{
+                    binding.cryptoErrorText.visibility=View.GONE
+                }
+            }
+
+        })
+
+        viewModel.cryptoLoading.observe(viewLifecycleOwner, Observer { loading ->
+            loading?.let {
+                if (it.data == true){
+                    binding.cryptoProgressBar.visibility=View.VISIBLE
+                    binding.recyclerView.visibility=View.GONE
+                    binding.cryptoErrorText.visibility=View.GONE
+                }
+                else{
+                    binding.cryptoProgressBar.visibility=View.GONE
+                }
+            }
+        })
     }
 
 
